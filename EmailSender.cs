7e975b07f0343412315f8fd9a57a997a03963174
Colart Lloyd <55253106+Lloydcol@users.noreply.github.com>
2021-06-18 16:08:56 +0200

    Init

---
+++ EmailSender.cs
@@ -0,0 +1,94 @@
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C.Tool.Mailer
+{
+    public class EmailSender : IEmailSender
+    {
+        private readonly EmailConfiguration _emailConfig;
+
+        public EmailSender(EmailConfiguration emailConfig)
+        {
+            _emailConfig = emailConfig;
+        }
+
+        public void SendEmail(Message message)
+        {
+            var emailMessage = CreateEmailMessage(message);
+
+            Send(emailMessage);
+        }
+
+        public async Task SendEmailAsync(Message message)
+        {
+            var mailMessage = CreateEmailMessage(message);
+            await SendAsync(mailMessage);
+        }
+
+        private MimeMessage CreateEmailMessage(Message message)
+        {
+            var emailMessage = new MimeMessage();
+            emailMessage.From.Add(new MailboxAddress(_emailConfig.From, _emailConfig.From));
+            emailMessage.To.Add(new MailboxAddress(message.To, message.To));
+            emailMessage.Subject = message.Subject;
+            emailMessage.Body = // new TextPart(MimeKit.Text.TextFormat.Text) { Text = message.Content };
+                                new TextPart(MimeKit.Text.TextFormat.Html) { Text = message.Content };
+
+            return emailMessage;
+        }
+
+        private void Send(MimeMessage mailMessage)
+        {
+            using (var client = new SmtpClient())
+            {
+                try
+                {
+                    client.Connect(_emailConfig.SmtpServer, _emailConfig.Port, false);
+                    //client.AuthenticationMechanisms.Remove("XOAUTH2");
+                    //client.Authenticate(_emailConfig.UserName, _emailConfig.Password);
+
+                    client.Send(mailMessage);
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+                finally
+                {
+                    client.Disconnect(true);
+                    client.Dispose();
+                }
+            }
+        }
+
+        private async Task SendAsync(MimeMessage mailMessage)
+        {
+            using (var client = new SmtpClient())
+            {
+                try
+                {
+                    await client.ConnectAsync(_emailConfig.SmtpServer, _emailConfig.Port, false);
+                    client.AuthenticationMechanisms.Remove("XOAUTH2");
+                    await client.AuthenticateAsync(_emailConfig.UserName, _emailConfig.Password);
+
+                    await client.SendAsync(mailMessage);
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+                finally
+                {
+                    await client.DisconnectAsync(true);
+                    client.Dispose();
+                }
+            }
+        }
+    }
+}
